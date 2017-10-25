f607c1f3670b97af4d08040453603764da3d4da1
Agnt45 <an160264@gmail.com>
2017-10-26 00:55:45 +0530

    Fifth Commit

--- app/src/main/java/com/agnt45/nearme/LoginActivity.java
+++ app/src/main/java/com/agnt45/nearme/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.agnt45.nearme;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button Login;
     private ProgressDialog progressDialog;
+    private TextView forgotPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +39,52 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setTitle("Logging In");
         progressDialog.setMessage("Please Wait...");
         progressDialog.setCanceledOnTouchOutside(false);
+        forgotPass = findViewById(R.id.FPASS);
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog d1 = new Dialog(LoginActivity.this);
+                d1.setContentView(R.layout.forgot_password_layout);
+                d1.show();
+                final TextInputLayout email = d1.findViewById(R.id.email_Input);
+                Button send = d1.findViewById(R.id.SendMailVerify);
+                send.setVisibility(View.INVISIBLE);
+                if(email.getEditText().getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Please Enter the Email to Continue",Toast.LENGTH_LONG).show();
 
+                }
+                else{
+                    send.setVisibility(View.VISIBLE);
+                    send.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            final ProgressDialog progressDialog1 =  new ProgressDialog(LoginActivity.this);
+                            progressDialog1.setTitle("Please Wait");
+                            progressDialog1.setMessage("Sending Verfication Link to"+email.getEditText().getText().toString());
+                            progressDialog1.setCanceledOnTouchOutside(false);
+                            progressDialog1.show();
+                            mAuth.sendPasswordResetEmail(email.getEditText().getText().toString())
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                progressDialog1.dismiss();
+                                                Toast.makeText(LoginActivity.this,"Verification  Link Sent Please Check your email",Toast.LENGTH_LONG).show();
+                                                email.getEditText().setText("");
+
+                                            }
+                                            else {
+                                                progressDialog1.hide();
+                                                Toast.makeText(LoginActivity.this,"Error in Sending Verification  Link Please check the email inputed",Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        }
+                    });
+                }
+
+            }
+        });
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
